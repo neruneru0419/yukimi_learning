67a8e5dc8f79a26072a2e94a72a3117191607445
neruneru0419 <neruneru55555@gmail.com>
2022-12-01 01:20:53 +0900

    classを削除

--- src/yukimi_twitter.rb
+++ src/yukimi_twitter.rb
@@ -5,6 +5,7 @@ require "oauth"
 require_relative "twitter/get_timeline"
 require_relative "twitter/get_tweet"
 require_relative "twitter/get_user_id"
+require_relative "twitter/get_user_timeline"
 require_relative "twitter/get_mention"
 require_relative "twitter/get_users"
 require_relative "twitter/post_tweet"
@@ -13,27 +14,14 @@ require_relative "twitter/post_reply"
 require_relative "twitter/delete_remove"
 
 module YukimiTwitter
-  class Twitter
-    include GetTimeline
-    include GetTweet
-    include GetUserId
-    include GetMention
-    include GetUsers
-    include PostTweet
-    include PostFavorite
-    include PostReply
-    include DeleteRemove
-
-    def initialize
-      @api_key = ENV["api_key"]
-      @api_key_secret = ENV["api_key_secret"]
-      @access_token = ENV["access_token"]
-      @access_token_secret = ENV["access_token_secret"]
-      @bearer_token = ENV["bearer_token"]
-      @client = OAuth::Consumer.new(@api_key, @api_key_secret, :site => 'https://api.twitter.com',
-                                                            :authorize_path => '/oauth/authenticate',
-                                                            :debug_output => false)
-      @credentials = OAuth::AccessToken.new(@client, @access_token, @access_token_secret)
-    end
-  end
+  include GetTimeline
+  include GetTweet
+  include GetUserTimeline
+  include GetUserId
+  include GetMention
+  include GetUsers
+  include PostTweet
+  include PostFavorite
+  include PostReply
+  include DeleteRemove
 end

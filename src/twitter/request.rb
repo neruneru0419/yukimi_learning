b11117cfb2ed904ab1edbe1eba44716880a4790c
neruneru0419 <neruneru55555@gmail.com>
2022-12-01 01:22:12 +0900

    認証に必要な情報を移植

--- src/twitter/request.rb
+++ src/twitter/request.rb
@@ -5,9 +5,18 @@ require 'oauth/request_proxy/typhoeus_request'
 
 module Request
   def oauth1_request(url, options)
-    access_token = @credentials
+    api_key = ENV["api_key"]
+    api_key_secret = ENV["api_key_secret"]
+    access_token = ENV["access_token"]
+    access_token_secret = ENV["access_token_secret"]
+    bearer_token = ENV["bearer_token"]
+    client = OAuth::Consumer.new(api_key, api_key_secret, :site => 'https://api.twitter.com',
+                                                          :authorize_path => '/oauth/authenticate',
+                                                          :debug_output => false)
+    credentials = OAuth::AccessToken.new(client, access_token, access_token_secret)
 
-    oauth_params = {:consumer => @client, :token => access_token}
+    access_token = credentials
+    oauth_params = {:consumer => client, :token => access_token}
   
     request = Typhoeus::Request.new(url, options)
     oauth_helper = OAuth::Client::Helper.new(request, oauth_params.merge(:request_uri => url))

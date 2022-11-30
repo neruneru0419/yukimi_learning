884637bfd65dc27e4f529471ee15563ebfb5a6d7
neruneru0419 <neruneru55555@gmail.com>
2022-12-01 01:23:21 +0900

    リクエストの際のオプションを追記

--- src/twitter/get_mention.rb
+++ src/twitter/get_mention.rb
@@ -1,12 +1,11 @@
 module GetMention
   def get_mention(user_id, format: "json")#"tweet_ids" must be a string. If more than one ID's are given, they must be comma separated
     query = {
-      "tweet.fields" => "attachments,author_id,conversation_id,created_at,entities,id,lang",
+      "tweet.fields" => "attachments,author_id,conversation_id,created_at,entities,id,lang,referenced_tweets",
       "user.fields" => "description",
-      "expansions" => "attachments.poll_ids,attachments.media_keys,author_id"            
+      "expansions" => "attachments.poll_ids,attachments.media_keys,author_id,in_reply_to_user_id"            
     }
     url = "https://api.twitter.com/2/users/#{user_id}/mentions?max_results=100&#{URI.encode_www_form(query)}"
-    p url
     options = {
       :method => :get,
       headers: {
--- src/twitter/get_timeline.rb
+++ src/twitter/get_timeline.rb
@@ -1,11 +1,11 @@
 module GetTimeline    
   def get_timeline(user_id, format: "json")#"tweet_ids" must be a string. If more than one ID's are given, they must be comma separated
     query = {
-      "tweet.fields" => "conversation_id,created_at,public_metrics,id,referenced_tweets",
+      "tweet.fields" => "conversation_id,created_at,public_metrics,id,referenced_tweets,reply_settings",
       "user.fields" => "description,username,url",
-      "expansions" => "author_id"            
+      "expansions" => "author_id"
     }
-    url = "https://api.twitter.com/2/users/#{user_id}/timelines/reverse_chronological?#{URI.encode_www_form(query)}"
+    url = "https://api.twitter.com/2/users/#{user_id}/tweets?#{URI.encode_www_form(query)}"
 
     options = {
       :method => :get,

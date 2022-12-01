b05bd5fc0fc47aaa4527cab926d47027c0568431
neruneru0419 <neruneru55555@gmail.com>
2022-12-02 00:35:48 +0900

    空白削除

--- src/twitter/delete_remove.rb
+++ src/twitter/delete_remove.rb
@@ -3,7 +3,6 @@ require_relative "request"
 module DeleteRemove
   include Request
   def delete_remove(user_id, target_user_id)
-
     url = "https://api.twitter.com/2/users/#{user_id}/following/#{target_user_id}"
 
     options = {

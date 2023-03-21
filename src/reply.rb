0098c1cd5667f71515aaa0bf6ba011e212c37979
neruneru0419 <neruneru55555@gmail.com>
2023-03-21 23:33:44 +0900

    max_result追加

--- src/reply.rb
+++ src/reply.rb
@@ -15,14 +15,16 @@ module LambdaFunction
   class Handler
     def self.process(event:, context:)
       user_id = get_user_id(UserName)["data"][0]["id"]
-      reply_data = get_mention(user_id)["data"]
+      max_results = 20
+      reply_data = get_mention(user_id, max_results)["data"]
       reply_data.each do |reply|
         reply_id = reply["id"]
         author_id = reply["author_id"]
         if reply_to_author?(author_id, reply_id, user_id)#もらったリプライのリプライを確認したい
-          mention_text = get_mention_text(author_id, user_id)
+          mention_text = get_mention_text(user_id)
           reply_text = change_yukimi(mention_text)
           post_reply(reply_text, reply_id)
+          puts(reply_text, reply_id)
         end
       end
     end
@@ -30,10 +32,9 @@ module LambdaFunction
 end
 
 def reply_to_author?(author_id, reply_id, user_id)
-  #リプライをもらったユーザーが受けたメンション一覧を取得し、riamuからのメンションだけを厳選する
-  #そのメンションが今回のリプライへの返信じゃないか確認する
   tw = []
-  yukimi_tweets = get_mention(author_id)["data"]
+  max_results = 50
+  yukimi_tweets = get_mention(author_id, max_results)["data"]
   yukimi_tweets.each do |mention|
     unless mention["referenced_tweets"].nil?
       if mention["referenced_tweets"][0]["id"] == reply_id
@@ -44,13 +45,14 @@ def reply_to_author?(author_id, reply_id, user_id)
   tw.empty?
 end
 
-def get_mention_text(author_id, user_id)
-  mention_data = get_user_timeline(author_id)["data"].sample
+def get_mention_text(user_id)
+  mention_data = get_home_timeline(user_id)["data"].sample
+  
   mention_text = mention_data["text"]
   mention_id = mention_data["author_id"]
   unless mention_text.include?("http") || mention_text.include?("#") || ngword?(mention_text) || mention_id == user_id
     mention_text
   else
-    get_mention_text(author_id, user_id)
+    get_mention_text(user_id)
   end
 end
--- src/twitter/get_mention.rb
+++ src/twitter/get_mention.rb
@@ -1,10 +1,10 @@
 module GetMention
-  def get_mention(user_id, format: "json")#"tweet_ids" must be a string. If more than one ID's are given, they must be comma separated
+  def get_mention(user_id, max_results)#"tweet_ids" must be a string. If more than one ID's are given, they must be comma separated
     query = {
       "tweet.fields" => "attachments,author_id,conversation_id,created_at,entities,id,lang,referenced_tweets",
       "user.fields" => "description",
       "expansions" => "attachments.poll_ids,attachments.media_keys,author_id,in_reply_to_user_id",
-      "max_results" => "100"
+      "max_results" => max_results
     }
     url = "https://api.twitter.com/2/users/#{user_id}/mentions?&#{URI.encode_www_form(query)}"
     options = {
